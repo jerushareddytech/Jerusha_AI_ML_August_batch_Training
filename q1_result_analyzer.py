5cb442e51664425e591f57e7b6c1ecab6acfa193
Jerusha reddy <jerushareddy2007@gmail.com>
2026-08-18 17:12:21 +0530

    Update q1_result_analyzer.py

--- q1_result_analyzer.py
+++ q1_result_analyzer.py
@@ -1,7 +1,6 @@
 def analyze_result(name, roll, marks):
     total = sum(marks)
     average = total / len(marks)
-
     if average >= 90:
         grade = "A"
     elif average >= 75:
@@ -12,26 +11,19 @@ def analyze_result(name, roll, marks):
         grade = "D"
     else:
         grade = "Fail"
-
     print(f"Student: {name} (Roll: {roll})")
     print(f"Total: {total}, Average: {average}")
     print(f"Grade: {grade}")
-
     below_40 = []
-
     for i in range(len(marks)):
         if marks[i] < 40:
             below_40.append(f"Subject {i + 1}")
-
     if below_40:
         print("Subjects below 40:", ", ".join(below_40))
     else:
         print("Subjects below 40: None")
-
-
 # Sample input
 name = "Aarav"
 roll = 101
 marks = [88.5, 35.0, 76.0, 92.5, 48.0]
-
 analyze_result(name, roll, marks)
